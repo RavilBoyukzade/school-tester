b7da1f08c9496a32fb4a37ded8ae4b70b0c58238
ravilboyukzade <ravil_1996@mail.ru>
2025-04-17 17:24:50 +0400

    Add AuthGuard to protect routes and configure navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 
-const routes: Routes = [{ path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) }, { path: 'teacher', loadChildren: () => import('./modules/teacher/teacher.module').then(m => m.TeacherModule) }, { path: 'student', loadChildren: () => import('./modules/student/student.module').then(m => m.StudentModule) }];
+const routes: Routes = [{ path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) }, {
+  path: 'teacher',
+  canActivate: [AuthGuard],
+  loadChildren: () =>
+    import('./modules/teacher/teacher.module').then((m) => m.TeacherModule),
+},
+{
+  path: 'student',
+  canActivate: [AuthGuard],
+  loadChildren: () =>
+    import('./modules/student/student.module').then((m) => m.StudentModule),
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../../../core/services/auth.service';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   standalone:false,
@@ -11,7 +12,7 @@ import { Router } from '@angular/router';
 export class LoginComponent {
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router,private toastr: ToastrService) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
@@ -24,9 +25,10 @@ export class LoginComponent {
       const result = this.authService.login(email, password);
       if (result) {
         const role = this.authService.getRole();
+        this.toastr.success('Успешный вход!');
         this.router.navigate([`/${role}`]);
       } else {
-        alert('Неверный логин или пароль');
+        this.toastr.error('Неверный логин или пароль');
       }
     }
   }
